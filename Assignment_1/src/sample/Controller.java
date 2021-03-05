d34e86d839c95add03912570953c4c8b468cfb0a
Jack Bowman <1bowmanjac@gmail.com>
2021-03-05 13:06:16 -0500

    began assignment 1

---
+++ Assignment_1/src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+public class Controller {
+    @FXML
+    public void initialize(){
+
+    }
+}
